09981f4c59320e46bb23fba354d78d8e0aee51b1
Louis Stevens <loustevenshere@gmail.com>
2024-08-24 12:42:06 -0400

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import MapComponent from "./MapComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Location Map</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
