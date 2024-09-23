7985489b552e919d812295c3e628b45f8d211f11
Louis Stevens <loustevenshere@gmail.com>
2024-09-23 12:49:01 -0400

    trying to fix marker display issue

--- src/GoogleMapComponent.js
+++ src/GoogleMapComponent.js
@@ -1,10 +1,5 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
-import {
-  GoogleMap,
-  LoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
+import { GoogleMap, LoadScript, InfoWindow } from "@react-google-maps/api";
 import axiosInstance from "./axiosconfig";
 
 import * as styles from "./GoogleMapStyles";
@@ -16,8 +11,7 @@ const GoogleMapComponent = () => {
   const [selectedLocation, setSelectedLocation] = useState(null); // active user selected location
   const [showLocationForm, setShowLocationForm] = useState(false); // InfoWindow state
   const [formData, setFormData] = useState({ title: "", description: "" }); // formData
-  const [loadError, setLoadError] = useState(null);
-  const [isMapReady, setIsMapReady] = useState(false); // To check if the map should render
+  // const [loadError, setLoadError] = useState(null);
   const [showInfoWindow, setShowInfoWindow] = useState(false);
   const [activeMarker, setActiveMarker] = useState(null);
 
@@ -30,19 +24,18 @@ const GoogleMapComponent = () => {
     fetchLocations();
   }, []);
 
-  const handleRetry = () => {
-    setLoadError(null);
-  };
+  // TODO: implement retry logic when map doesnt load properly
+  // const handleRetry = () => {
+  //   setLoadError(null);
+  // };
 
   const fetchLocations = async () => {
     // fetches saved locations from BE
     try {
       const response = await axiosInstance.get("/api/locations");
       setLocationMarkers(response.data);
-      setIsMapReady(true); // Set map ready when data is fetched
     } catch (error) {
       console.error("Error fetching locations", error);
-      setIsMapReady(true); // Set map ready when data is fetched
     }
   };
 
@@ -85,113 +78,104 @@ const GoogleMapComponent = () => {
 
   return (
     <div style={styles.mapWrapperStyle}>
-      {loadError ? (
-        <div>
-          <p>Error loading map. Please try again.</p>
-          <button onClick={handleRetry} style={styles.buttonStyle}>
-            Load Map
-          </button>
-        </div>
-      ) : (
-        <LoadScript
-          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-          onError={(error) => {
-            setLoadError(error);
-            console.error("Error loading google maps", error);
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        libraries={["marker"]}
+        onError={(error) => {
+          console.error("Error loading google maps", error);
+        }}
+      >
+        <GoogleMap
+          mapContainerStyle={styles.mapContainerStyle}
+          zoom={13}
+          center={mapStartPoint}
+          onLoad={onLoad}
+          onClick={handleMapClick}
+          options={{
+            disableDefaultUI: true,
+            styles: mapFeatures,
+            mapId: "3de749adcc419c15",
           }}
         >
-          <GoogleMap
-            mapContainerStyle={styles.mapContainerStyle}
-            zoom={13}
-            center={mapStartPoint}
-            onLoad={onLoad}
-            onClick={handleMapClick}
-            options={{
-              disableDefaultUI: true,
-              styles: mapFeatures,
-            }}
-          >
-            {locationMarkers.map((marker, index) => {
-              console.log(marker);
-              return (
-                <Marker
-                  key={index}
-                  position={{ lat: marker.latitude, lng: marker.longitude }}
-                  onClick={(event) => {
-                    setActiveMarker(marker);
-                    setShowInfoWindow(true);
-                  }}
-                ></Marker>
-              );
-            })}
-
-            {showInfoWindow && activeMarker && (
-              <InfoWindow
-                position={{
-                  lat: activeMarker.latitude,
-                  lng: activeMarker.longitude,
+          {/* {locationMarkers.map((marker, index) => {
+            return (
+              <AdvancedMarkerElement
+                key={index}
+                position={{ lat: marker.latitude, lng: marker.longitude }}
+                onClick={(event) => {
+                  setActiveMarker(marker);
+                  setShowInfoWindow(true);
                 }}
-                onCloseClick={() => {
-                  setActiveMarker(null);
-                  setShowInfoWindow(false);
-                }}
-              >
-                <div style={styles.infoWindowStyle}>
-                  <h3 style={styles.headingStyle}>{activeMarker.title}</h3>
-                  <p>{activeMarker.description}</p>
-                </div>
-              </InfoWindow>
-            )}
+              ></AdvancedMarkerElement>
+            );
+          })} */}
 
-            {/* Show the form as an InfoWindow when a location is selected */}
-            {showLocationForm && selectedLocation && (
-              <InfoWindow
-                position={selectedLocation}
-                onCloseClick={() => setShowLocationForm(false)}
-              >
-                <div style={styles.infoWindowStyle}>
-                  <h3 style={styles.headingStyle}>Add a Location</h3>
-                  <form onSubmit={handleFormSubmit} style={styles.formStyle}>
-                    <div style={styles.formGroupStyle}>
-                      <label style={styles.labelStyle}>Title:</label>
-                      <input
-                        type="text"
-                        value={formData.title}
-                        onChange={(e) =>
-                          setFormData({ ...formData, title: e.target.value })
-                        }
-                        required
-                        style={styles.inputStyle}
-                      />
-                    </div>
-                    <div style={styles.formGroupStyle}>
-                      <label style={styles.labelStyle}>Description:</label>
-                      <textarea
-                        value={formData.description}
-                        onChange={(e) =>
-                          setFormData({
-                            ...formData,
-                            description: e.target.value,
-                          })
-                        }
-                        required
-                        style={{
-                          ...styles.inputStyle,
-                          height: "90px",
-                          resize: "none",
-                        }}
-                      />
-                    </div>
-                    <button type="submit" style={styles.buttonStyle}>
-                      Add Location
-                    </button>
-                  </form>
-                </div>
-              </InfoWindow>
-            )}
-          </GoogleMap>
-        </LoadScript>
-      )}
+          {showInfoWindow && activeMarker && (
+            <InfoWindow
+              position={{
+                lat: activeMarker.latitude,
+                lng: activeMarker.longitude,
+              }}
+              onCloseClick={() => {
+                setActiveMarker(null);
+                setShowInfoWindow(false);
+              }}
+            >
+              <div style={styles.infoWindowStyle}>
+                <h3 style={styles.headingStyle}>{activeMarker.title}</h3>
+                <p>{activeMarker.description}</p>
+              </div>
+            </InfoWindow>
+          )}
+
+          {/* Show the form as an InfoWindow when a location is selected */}
+          {showLocationForm && selectedLocation && (
+            <InfoWindow
+              position={selectedLocation}
+              onCloseClick={() => setShowLocationForm(false)}
+            >
+              <div style={styles.infoWindowStyle}>
+                <h3 style={styles.headingStyle}>Add a Location</h3>
+                <form onSubmit={handleFormSubmit} style={styles.formStyle}>
+                  <div style={styles.formGroupStyle}>
+                    <label style={styles.labelStyle}>Title:</label>
+                    <input
+                      type="text"
+                      value={formData.title}
+                      onChange={(e) =>
+                        setFormData({ ...formData, title: e.target.value })
+                      }
+                      required
+                      style={styles.inputStyle}
+                    />
+                  </div>
+                  <div style={styles.formGroupStyle}>
+                    <label style={styles.labelStyle}>Description:</label>
+                    <textarea
+                      value={formData.description}
+                      onChange={(e) =>
+                        setFormData({
+                          ...formData,
+                          description: e.target.value,
+                        })
+                      }
+                      required
+                      style={{
+                        ...styles.inputStyle,
+                        height: "90px",
+                        resize: "none",
+                      }}
+                    />
+                  </div>
+                  <button type="submit" style={styles.buttonStyle}>
+                    Add Location
+                  </button>
+                </form>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 };
